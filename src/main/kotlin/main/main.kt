04f43f124f5a7866eb5a099279d735ee282477f7
Lucas Palomo <devlpalomo@gmail.com>
2020-01-19 11:32:51 -0300

    alteracao na main

--- src/main/kotlin/main/main.kt
+++ src/main/kotlin/main/main.kt
@@ -6,7 +6,7 @@ import org.json.simple.JSONObject
 
 fun main(args: Array<String>) {
 
-    val palavras = JSONArray()
+//    val palavras = JSONArray()
     val crawlerDicio = CrawlDicio();
     val crawlerPensador = CrawlPensador()
     val crawlerSinonimos = CrawlSinonimos()
@@ -14,17 +14,16 @@ fun main(args: Array<String>) {
     val crawlNomes = CrawlNomes()
     val crawlInformal = CrawlInformal()
 
-    for (word in listOf("feliz","cano","beca")) {
+    for (word in listOf("chuca","xuca","pau cagado")) {
 
         val palavra = crawlerDicio.cat(word)
 //        val palavra = JSONObject()
 
-//        palavra.put("informal",crawlInformal.cat(word))
-
-//        palavra.put("pessoa",crawlNomes.getNome(word))
-//        palavra.put("sinonimos",crawlerSinonimos.getSinonimos(word))
-//        palavra.put("antonimos",crawlAntonimos.getAntonimos(word))
-//        palavra.put("frases",crawlerPensador.getFrases(word))
+        palavra.put("informal",crawlInformal.cat(word))
+        palavra.put("pessoa",crawlNomes.getNome(word))
+        palavra.put("sinonimos",crawlerSinonimos.getSinonimos(word))
+        palavra.put("antonimos",crawlAntonimos.getAntonimos(word))
+        palavra.put("frases",crawlerPensador.getFrases(word))
         println(palavra.toJSONString() + "\n")
     }
 
